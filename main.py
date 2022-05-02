142326f57a257302f394a220048b951c6bceba4c
Radeww <104791372+Radeww@users.noreply.github.com>
2022-05-02 21:56:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import colorama
+import os
+import sys
+import time
+from colorama import Fore
+
+
+os.system('title Fortnite Account Puller')
+os.system('cls')
+user = input(Fore.CYAN + "Enter A Epic Games Username: ")
+input(Fore.MAGENTA + "Theards: ")
+print(Fore.RED + "Start Pulling "+ Fore.GREEN + user +Fore.YELLOW)
+os.system('title Pulling Account...')
+for x in range(4900):
+  print(x)
+  time.sleep(0.4)
